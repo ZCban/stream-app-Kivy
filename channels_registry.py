af853a3b0bd820d008891fd73f4f2a60748e4286
ZCban <100644497+ZCban@users.noreply.github.com>
2025-04-06 18:37:34 +0200

    Update channels_registry.py

--- channels_registry.py
+++ channels_registry.py
@@ -1,11 +1,13 @@
 # channels_registry.py
 
 # Importa i tuoi plugin (sorgenti dei canali)
-from plugins import guardaserietvpro , altadefinizione , calcioGA
+from plugins import guardaserietvpro,altadefinizione,calcioGA,SportZone,CalcioStreamingLat
 
 # Lista di canali disponibili
 CHANNELS = [
     guardaserietvpro,
     altadefinizione,
     calcioGA,
+    SportZone,
+    #CalcioStreamingLat, #non funzionante disattivato 
 ]
